383a85d9ea170dd336329b724e2fd4f40e7190b0
Cardoso88 <alexmcardoso@gmail.com>
2024-02-06 22:30:02 -0300

    gi

--- easy/day-of-programmer.js
+++ easy/day-of-programmer.js
@@ -17,21 +17,3 @@ function dayOfProgrammer(year) {
 
 
 console.log(dayOfProgrammer(1918))
-
-
-
-/*
-def dayOfProgrammer(year):
-    if 1700 <= year <= 1917:
-        if year % 4 == 0:
-            return f"12.09.{str(year)}"
-        else:
-            return f"13.09.{str(year)}"
-    elif year == 1918:
-        return f"26.09.{str(year)}"
-    else:
-        if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
-            return f"12.09.{str(year)}"
-        else:
-            return f"13.09.{str(year)}"
-*/
