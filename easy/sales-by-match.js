bf6789ffb4188d896682e1efa0ce32f4de83edf5
Cardoso88 <alexmcardoso@gmail.com>
2024-02-09 14:49:21 -0300

    easy - sales-by-match

--- easy/sales-by-match.js
+++ easy/sales-by-match.js
@@ -1,13 +1,15 @@
 function sockMerchant(n, ar) {
-  var pairMap = new Map()
-  var pairs = 0
-  ar.forEach(item => {
-    pairMap.set(item, pairMap.get(item) ? pairMap.get(item) + 1 : 1)
-  });
-  pairMap.forEach((value, key) => {
-    pairs += Math.floor(value / 2)
-  })
-  return pairs  
+  var a = Array.from(new Set(ar))
+  console.log(a)
+  // var pairMap = new Map()
+  // var pairs = 0
+  // ar.forEach(item => {
+  //   pairMap.set(item, pairMap.get(item) ? pairMap.get(item) + 1 : 1)
+  // });
+  // pairMap.forEach((value, key) => {
+  //   pairs += Math.floor(value / 2)
+  // })
+  // return pairs  
 }
 
 console.log(sockMerchant(7, [10,20,20,10,10,30,50,10,20]))
